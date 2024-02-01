8d54a6f7eb01ca48bd8a73165486f3e6ecae7d97
Asanaliev Temirlan <tonyadam050607@gmail.com>
2024-02-01 17:47:29 +0600

    Дз 8

--- app/src/main/java/com/example/calculator_hw_07/MainActivity.java
+++ app/src/main/java/com/example/calculator_hw_07/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.calculator_hw_07;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
@@ -14,15 +16,19 @@ public class MainActivity extends AppCompatActivity {
     private int secondOperand;
     private boolean isOperationClick;
     private String operator;
+    private Button buttonWhite = findViewById(R.id.button_white);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.text_view);
+
     }
 
     public void oneNumberClick(View view) {
+        Button buttonWhite = findViewById(R.id.button_white);
+
         if (view.getId() == R.id.btn_clear) {
             textView.setText("0");
         } else {
@@ -33,30 +39,40 @@ public class MainActivity extends AppCompatActivity {
                 textView.append(text);
             }
             isOperationClick = false;
-        }
+
+        }buttonWhite.setAlpha(0);
     }
 
     public void oneOperationClick(View view) {
         String currentText = textView.getText().toString();
         isOperationClick = true;
+        Button buttonWhite = findViewById(R.id.button_white);
 
         if (view.getId() == R.id.bt_plus) {
             firstOperand = Integer.parseInt(currentText);
             operator = "+";
+            buttonWhite.setAlpha(0);
         } else if (view.getId() == R.id.bt_minus) {
             firstOperand = Integer.parseInt(currentText);
             operator = "-";
+            buttonWhite.setAlpha(0);
         } else if (view.getId() == R.id.bt_division) {
             firstOperand = Integer.parseInt(currentText);
             operator = "/";
+            buttonWhite.setAlpha(0);
         } else if (view.getId() == R.id.bt_multiplication) {
             firstOperand = Integer.parseInt(currentText);
             operator = "*";
+            buttonWhite.setAlpha(0);
         } else if (view.getId() == R.id.bt_equal) {
             secondOperand = Integer.parseInt(currentText);
             int result = calculateResult(firstOperand, secondOperand, operator);
             textView.setText(String.valueOf(result));
             isOperationClick = false;
+
+            buttonWhite.setAlpha(1);
+
+
         }
     }
 
@@ -78,4 +94,5 @@ public class MainActivity extends AppCompatActivity {
                 return 0;
         }
     }
+
 }
